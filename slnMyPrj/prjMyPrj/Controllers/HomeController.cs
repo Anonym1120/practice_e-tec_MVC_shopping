ef5926cc9539beb6a78b69c4cbf9cc9ddd9e33f6
Anonym <anonym1120@gmail.com>
2021-02-24 18:11:24 +0800

    商品列表與使用者登入的前端畫面完成

--- slnMyPrj/prjMyPrj/Controllers/HomeController.cs
+++ slnMyPrj/prjMyPrj/Controllers/HomeController.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using prjMyPrj.Models;
+
 namespace prjMyPrj.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        dbmyDBEntities db = new dbmyDBEntities();
+
+        public ActionResult SignOut()
+        {
+            return RedirectToAction("Index");
+        }
+
+        
+
+        public ActionResult LogIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(tMember m)
+        {
+            
+            return View();
+        }
+        public ActionResult Index()
+        {
+            var list = db.tProduct.ToList();
+            return View(list);
         }
 
         public ActionResult About()
